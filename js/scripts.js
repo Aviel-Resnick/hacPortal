cc1d47066eb107eea6570e71e13d778f2f38983e
Aviel Resnick <aviel.resnick@gmail.com>
2019-03-12 19:14:46 -0400

    Sample UI

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+function main() {
+    var name = document.getElementById("user").value;
+    var pass = document.getElementById("pass").value;
+    alert("Username: " + name + " " + "Password: " + pass);
+}
