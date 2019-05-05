bcdf4004ea7b7ee950fc260b6c66608569e80896
Phillip Grabovsky <phillip.grabovsky@gmail.com>
2019-05-04 23:08:25 -0400

    Firebase test

--- js/login.js
+++ js/login.js
@@ -1,6 +1,19 @@
 function main() {
     var name = document.getElementById("user").value;
     var pass = document.getElementById("pass").value;
-    alert("Username: " + name + " " + "Password: " + pass);
+    alert("Username: " + name + " " + "Password: " + pass +
+   "your password is being used as your grades for testing purposes.");
+
+   // Get a reference to the database service
+   var database = firebase.database();
+
+   writeUserData(85);
+
     window.location.href = "dispGrades.html";
+}
+
+function writeUserData(userId, grade) {
+ firebase.database().ref('User 1/').set({
+   Grade: grade
+ });
 }
