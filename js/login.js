fd9a4cc5c99003ed153e4ede5dcfa5eeded29ba9
Phillip Grabovsky <phillip.grabovsky@gmail.com>
2019-05-04 23:43:27 -0400

    Push it real good pt 2

--- js/login.js
+++ js/login.js
@@ -1,15 +1,29 @@
-function main() {
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDXyzlPxV14EIPnskehmxNo5uYNqOE8e30",
+  authDomain: "home-access-access.firebaseapp.com",
+  databaseURL: "https://home-access-access.firebaseio.com",
+  projectId: "home-access-access",
+  storageBucket: "home-access-access.appspot.com",
+  messagingSenderId: "396567472683",
+  appId: "1:396567472683:web:538be5e028fa402a"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the database service
+var database = firebase.database();
+
+
+function submittedData() {
     var name = document.getElementById("user").value;
     var pass = document.getElementById("pass").value;
-    alert("Username: " + name + " " + "Password: " + pass +
-   "your password is being used as your grades for testing purposes.");
+    alert("Username: " + name + " " + "Password: " + pass);
 
-   // Get a reference to the database service
-   var database = firebase.database();
-
+   console.log("changing grades!")
    writeUserData(85);
 
-    window.location.href = "dispGrades.html";
+   window.location.href = "dispGrades.html";
 }
 
 function writeUserData(userId, grade) {
