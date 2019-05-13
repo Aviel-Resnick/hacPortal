b55a47910c62914eb5b1707146c5ff1457f4cb4e
Mark Klinchin <43303597+nexen01@users.noreply.github.com>
2019-05-12 23:11:11 -0400

    Made pupeteer click on classwork
    Not much was done, but I did do my research. I will add more tomorrow afternoon. I just wanted to show you guys that I am working on this.

--- js/login.js
+++ js/login.js
@@ -15,11 +15,11 @@ function launcher(){
 }
 
 //Log into HAC
-/*const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer');
 
 (async () => {
-  var username = "something"; //Change the value and place -> have it retrieved from the webpage
-  var password = "something"; //Change the value and place -> have it retrieved from the webpage
+  var username = "3711"; //Change the value and place -> have it retrieved from the webpage
+  var password = "lm5d6765"; //Change the value and place -> have it retrieved from the webpage
   const browser = await puppeteer.launch({headless: true})
   const page = await browser.newPage()
   await page.goto('https://hac40.eschoolplus.powerschool.com/HAC4_001/Account/LogOn?ReturnUrl=%2fHAC4_001%3fSiteCode%3dlmrlive&SiteCode=lmrlive')
@@ -34,9 +34,22 @@ function launcher(){
     page.click('[class="sg-button sg-logon-button ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only"]'),
   ]);
 
+  //Click on classwork
+  await page.click('[id="hac-Classes"]');
+
+  //wait for the page to load
+  await Promise.all([
+    page.waitForNavigation({ waitUntil: 'load' }),
+
+  ]);
+
+  //Click on "full view" then "collapse all"
+  //await page.click('[id="btnView"]');
+  //await page.click('[id="plnMain_btnCollapseExpand"]');
+
+
   //Take a screenshot of what the headless chrome displays
-  await page.screenshot({path: 'mainpage.png'});
+  await page.screenshot({path: 'mainpage.png', fullPage: true});
   browser.close()
   console.log("Done")
 })();
-*/
